0526f229ee4fd8d9de0bdc0f0a91c282987df08f
TrungHieu0102 <hatrunghieu121@gmail.com>
2024-12-09 21:11:04 +0700

    delete some unused routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import { Link } from "react-router-dom";
 import "./App.css";
+import { ChooseBrand } from "./components/ChooseBrand";
 function App() {
   return (
-    <div style={{display:"flex", gap:"4px", flexDirection:"column"}}>
-     <Link to="/choosebrand" >Choose brand</Link>
-     <Link to="/product">Product</Link>
-     <Link to="/state">State</Link>
-     <Link to="/render-list">Reder List</Link>
-     <Link to="/form">Form</Link>
-     <Link to="/login">Login</Link>
-
-
-    </div>
+    <ChooseBrand/>
   );
 }
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,42 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Product from "./pages/Product.tsx";
-import { ChooseBrand } from "./components/ChooseBrand.tsx";
-import State from "./pages/State.tsx";
-import RenderList from "./pages/RenderList.tsx";
-import Form from "./pages/Form.tsx";
-import Login from "./pages/Login.tsx";
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/product",
-    element: <Product />,
-  },
-  {
-    path: "/choosebrand",
-    element: <ChooseBrand />,
-  },
-  {
-    path: "/state",
-    element: <State />,
-  },
-  {
-    path: "/render-list",
-    element: <RenderList />,
-  },
-  {
-    path: "/form",
-    element: <Form />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  
+  }, 
 ]);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
