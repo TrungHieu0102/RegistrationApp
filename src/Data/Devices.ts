a13934b4c04121d710264d81ac530401d2484106
TrungHieu0102 <hatrunghieu121@gmail.com>
2025-01-07 18:01:33 +0700

    update images

--- src/Data/Devices.ts
+++ src/Data/Devices.ts
@@ -1,3 +1,4 @@
+import images from "../assets/images";
 export interface Devices {
   id: number;
   brandId: number;
@@ -5,18 +6,53 @@ export interface Devices {
   image: string;
   isActive: boolean;
 }
-    const devices: Devices[] = [  
-      { id: 1, brandId: 1, name: "AirPods", image: "airpods.png", isActive: true },
-      { id: 2, brandId: 1, name: "Apple TV", image: "apple-tv.png", isActive: true },
-      { id: 3, brandId: 1, name: "iPhone", image: "iphone.png", isActive: true },
-      { id: 4, brandId: 1, name: "MacBook Pro", image: "macbook-pro.png", isActive: true },
-      { id: 5, brandId: 2, name: "Galaxy S23", image: "galaxy-s23.png", isActive: true },
-      { id: 6, brandId: 2, name: "Galaxy Note 20", image: "galaxy-note-20.png", isActive: true },
-      { id: 7, brandId: 2, name: "Galaxy Tab", image: "galaxy-tab.png", isActive: true },
-      { id: 8, brandId: 3, name: "Mi Band", image: "mi-band.png", isActive: true },
-      { id: 9, brandId: 3, name: "Redmi Note", image: "redmi-note.png", isActive: true },
-      { id: 10, brandId: 3, name: "Mi Pad", image: "mi-pad.png", isActive: true },
-    ];
-    
-    export default devices;
-    
+const devices: Devices[] = [
+  { id: 1, brandId: 1, name: "AirPods", image: images.airpods, isActive: true },
+  {
+    id: 2,
+    brandId: 1,
+    name: "Apple TV",
+    image: images.appletv,
+    isActive: true,
+  },
+  { id: 3, brandId: 1, name: "iPhone", image: images.iphone, isActive: true },
+  {
+    id: 4,
+    brandId: 1,
+    name: "MacBook Pro",
+    image: images.macbook,
+    isActive: true,
+  },
+  {
+    id: 5,
+    brandId: 2,
+    name: "Galaxy S23",
+    image: images.samsungone,
+    isActive: true,
+  },
+  {
+    id: 6,
+    brandId: 2,
+    name: "Galaxy Note 20",
+    image: images.samsungtwo,
+    isActive: true,
+  },
+  {
+    id: 7,
+    brandId: 2,
+    name: "Galaxy Tab",
+    image: images.tabletSamsung,
+    isActive: true,
+  },
+  { id: 8, brandId: 3, name: "Mi Band", image: images.miband, isActive: true },
+  {
+    id: 9,
+    brandId: 3,
+    name: "Redmi Note",
+    image: images.redmi,
+    isActive: true,
+  },
+  { id: 10, brandId: 3, name: "Mi Pad", image: images.mipad, isActive: true },
+];
+
+export default devices;
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,12 +1,32 @@
 import apple from "./apple.png";
 import samsung from "./samsung.png";
 import xiaomi from "./xiaomi.png";
-import iconmap from "./icommap.png"
+import iconmap from "./icommap.png";
+import airpods from "../device/airpods.png";
+import appletv from "../device/appletv.png";
+import iphone from "../device/iphone.png";
+import macbook from "../device/macbook.png";
+import miband from "../device/miband.png";
+import mipad from "../device/mipad.png";
+import redmi from "../device/redmi.png";
+import samsungone from "../device/samsung.png";
+import samsungtwo from "../device/samsung2.png";
+import tabletSamsung from "../device/tablet-samsung.png";
 const images = {
   apple,
   samsung,
   xiaomi,
-  iconmap
+  iconmap,
+  airpods,
+  appletv,
+  iphone,
+  macbook,
+  miband,
+  mipad,
+  redmi,
+  samsungone,
+  samsungtwo,
+  tabletSamsung,
 };
 
 export default images;
